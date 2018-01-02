3141d57af4b06ae6071a90aaebca868a26e7adab
David Bonner <534654+davidianbonner@users.noreply.github.com>
2018-01-02 01:25:46 +0000

    Update brew-cask.sh

--- install/brew-cask.sh
+++ install/brew-cask.sh
@@ -70,3 +70,23 @@ apps=(
 )
 
 brew cask install "${apps[@]}"
+
+# Install Browser Caskroom
+
+brew tap caskroom/fonts
+
+apps=(
+    font-fira-code
+    font-montserrat
+    font-muli
+    font-playfair-display
+    font-playfair-display-sc
+    font-quicksand
+    font-source-sans-pro
+    font-ubuntu
+    font-dejavu-sans
+    font-fira-code
+    font-josefin-sans
+)
+
+brew cask install "${apps[@]}"
