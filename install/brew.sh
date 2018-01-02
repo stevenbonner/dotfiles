6e8d4b8e71ec9119b8971a18c93ea0dda66dfe0d
David Bonner <534654+davidianbonner@users.noreply.github.com>
2018-01-02 01:00:18 +0000

    Update brew.sh

--- install/brew.sh
+++ install/brew.sh
@@ -1,8 +1,8 @@
 # Install Homebrew
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew tap homebrew/versions
-brew tap homebrew/dupes
+#brew tap homebrew/versions
+#brew tap homebrew/dupes
 brew tap Goles/battery
 brew update
 brew upgrade
