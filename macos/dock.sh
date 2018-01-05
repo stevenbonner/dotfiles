9297d69ef6784304579dccbfbbade2c15c5389ee
David Bonner <534654+davidianbonner@users.noreply.github.com>
2018-01-05 12:09:11 +0000

    Update dock.sh

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
+dockutil --no-restart --add "/Applications/System Preferences.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
 dockutil --no-restart --add "/Applications/Firefox.app"
 dockutil --no-restart --add "/Applications/Safari.app"
 dockutil --no-restart --add "/Applications/Calendar.app"
 dockutil --no-restart --add "/Applications/iterm.app"
-dockutil --no-restart --add "/Applications/System Preferences.app"
+dockutil --no-restart --add "/Applications/Sublime Text.app"
+dockutil --no-restart --add "/Applications/Visual\ Studio\ Code.app"
+dockutil --no-restart --add "/Applications/Sequel\ Pro.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
 dockutil --no-restart --add "/Applications/Slack.app"
-dockutil --no-restart --add "/Applications/Sublime Text.app"
 dockutil --no-restart --add "/Applications/Messages.app"
 
 killall Dock
