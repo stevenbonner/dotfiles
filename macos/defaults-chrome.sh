2b4e94b4f484aa4ba3534577fc8176e582d794e3
David Bonner <534654+davidianbonner@users.noreply.github.com>
2018-01-09 10:46:44 +0000

    Update defaults-chrome.sh

--- macos/defaults-chrome.sh
+++ macos/defaults-chrome.sh
@@ -1,10 +1,10 @@
 # Disable swipe navigation
-defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool false
-defaults write com.google.Chrome.canary AppleEnableSwipeNavigateWithScrolls -bool false
+defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool true
+defaults write com.google.Chrome.canary AppleEnableSwipeNavigateWithScrolls -bool true
 
 # Use the system-native print preview dialog
-defaults write com.google.Chrome DisablePrintPreview -bool true
-defaults write com.google.Chrome.canary DisablePrintPreview -bool true
+defaults write com.google.Chrome DisablePrintPreview -bool false
+defaults write com.google.Chrome.canary DisablePrintPreview -bool false
 
 # Expand the print dialog by default
 defaults write com.google.Chrome PMPrintingExpandedStateForPrint2 -bool true
